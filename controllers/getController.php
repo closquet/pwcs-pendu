7f476d40a4488db49909ad7a38114cd43da5d52c
Eric Closquet <closquet.eric@live.be>
2017-03-09 06:06:14 +0100

    session ok

--- controllers/getController.php
+++ controllers/getController.php
@@ -1,66 +1,24 @@
 <?php
-/**
- * $wordFound est un indicateur booléen du fait que le mot a été trouvé ou pas
- * @var boolean
- */
 $wordFound = false;
 
-/**
- * Le nombre d’essais qui reste au joueur pour trouver le mot. 
- * @var integer
- */
 $remainingTrials = MAX_TRIALS;
 
-/**
- * Le nombre d’essais infructueux déjà fait
- * @var integer
- */
 $trials = 0;
-setcookie('trials', $trials);
+$_SESSION['trials'] = $trials;
 
-/**
- * Les lettres déjà essayées
- * @var string
- */
 $triedLetters = '0';
-setcookie('triedLetters', $triedLetters);
+$_SESSION['triedLetters'] = $triedLetters;
 
-/**
- * Un tableau des lettres utilisables pour faire le select
- * @var array
- */
 $lettersArray = getLettersArray();
+$_SESSION['lettersArray'] = $lettersArray;
 
-/**
- * Une chaîne qui représente le tableau des lettres
- * @var string
- */
-$serializedLetters = getSerializedLetters($lettersArray);
-setcookie('serializedLetters', $serializedLetters);
-
-/**
- * La position du mot à trouver dans le tableau
- * @var integer
- */
 $wordIndex = getRandomIndex($wordsArray);
-setcookie('wordIndex', $wordIndex);
+$_SESSION['wordIndex'] = $wordIndex;
 
-/**
- * Le mot à trouver
- * @var string
- */
 $word = getWord($wordsArray, $wordIndex);
 
-/**
- * le nombre de lettres du mot
- * @var integer
- */
 $lettersCount = strlen($word);
 
-/**
- * La chaîne fantôme qui masque les lettres du mot avec un caractère de remplacement
- * @var string
- */
 $replacementString = getReplacementString($lettersCount);
-setcookie('replacementString', $replacementString);
+$_SESSION['replacementString'] = $replacementString;
 
--- controllers/postController.php
+++ controllers/postController.php
@@ -1,62 +1,20 @@
 <?php    
-/**
- * $wordFound est un indicateur booléen du fait que le mot a été trouvé ou pas
- * @var boolean
- */
 $wordFound = false;
 
-/**
- * Le nombre d’essais infructueux déjà fait
- * @var integer
- */
-$trials = intval($_COOKIE['trials']);
+$trials = intval($_SESSION['trials']);
 
-/**
- * Une chaîne qui représente le tableau des lettres
- * @var string
- */
-$serializedLetters = $_COOKIE['serializedLetters'];
+$lettersArray = $_SESSION['lettersArray'];
 
-/**
- * Un tableau des lettres utilisables pour faire le select
- * @var array
- */
-$lettersArray = getUnserializedLetters($serializedLetters);
+$wordIndex = intval($_SESSION['wordIndex']);
 
-/**
- * La position du mot à trouver dans le tableau
- * @var int
- */
-$wordIndex = intval($_COOKIE['wordIndex']);
-
-/**
- * Le mot à trouver
- * @var string
- */
 $word = getWord($wordsArray, $wordIndex);
 
-/**
- * le nombre de lettres du mot
- * @var int
- */
 $lettersCount = strlen($word);
 
-/**
- * La chaîne fantôme qui masque les lettres du mot avec un caractère de remplacement
- * @var string
- */
-$replacementString = $_COOKIE['replacementString'];
+$replacementString = $_SESSION['replacementString'];
 
-/**
- * Les lettres déjà essayées
- * @var string
- */
-$triedLetters = $_COOKIE['triedLetters'];
+$triedLetters = $_SESSION['triedLetters'];
 
-/**
- * La lettre qui vient d’être essayée
- * @var string
- */
 $triedLetter = $_POST['triedLetter'];
 
 if ($triedLetters == '0'){
@@ -64,8 +22,6 @@ if ($triedLetters == '0'){
 }
 $triedLetters .= $triedLetter; // Modification de la chaîne des lettres déjà essayées en y ajoutant la nouvelle essayée par le joueur
 $lettersArray[$triedLetter] = false; // Modification du statut de la lettre qui vient d’être essayée. Son statut est mis à false dans le tableau $lettersArray
-$serializedLetters = getSerializedLetters($lettersArray); // Transformation du tableau des lettres en une chaine qui le représente
-
 
 $letterFound = false;
 $letterFound = testLetter($word, $triedLetter);
@@ -75,7 +31,7 @@ if ($letterFound === false){
     foreach (str_split($word) as $index => $letter){
         if ($letter == $triedLetter){
             $replacementString[$index] = $letter;
-            setcookie('replacementString', $replacementString);
+            $_SESSION['replacementString'] = $replacementString;
             if ($word == $replacementString){
                 $wordFound = true;
             }
@@ -83,6 +39,6 @@ if ($letterFound === false){
     }
 }
 $remainingTrials = MAX_TRIALS - $trials;
-setcookie('serializedLetters', $serializedLetters);
-setcookie('trials', $trials);
-setcookie('triedLetters', $triedLetters);
+$_SESSION['lettersArray'] = $lettersArray;
+$_SESSION['trials'] = $trials;
+$_SESSION['triedLetters'] = $triedLetters;
